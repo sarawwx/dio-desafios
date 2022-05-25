e2d80d1b01ca4d205c6dc3cc82a351305472d622
Sara L <wwwsaramilk@gmail.com>
2022-05-25 20:09:34 -0300

    adionando script

---
+++ javascript/javascript-assincrono/assets/js/script.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest';
+
+const getCats = async () => {
+   try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+
+    return json.webpurl;
+    } 
+   catch (e) {
+    console.log(e.message);
+   }
+}
